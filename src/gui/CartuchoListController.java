d4522b6116df23deef1582d9a3cddbe13bd960b6
elualves <elualvess@gmail.com>
2019-08-20 21:15:05 -0300

    CartuchoFormController

--- src/gui/CartuchoListController.java
+++ src/gui/CartuchoListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Cartucho;
 import model.services.CartuchoService;
@@ -72,8 +81,9 @@ public class CartuchoListController implements Initializable {
 	private ObservableList<Cartucho> obsList;
 	
 	@FXML
-	public void onBtNovoAction() {
-		System.out.println("onBtNovoAction");
+	public void onBtNovoAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/CartuchoForm.fxml", parentStage);
 	}
 	
 	public void setCartuchoService(CartuchoService service) {
@@ -113,7 +123,26 @@ public class CartuchoListController implements Initializable {
 		List<Cartucho> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewCartucho.setItems(obsList);
+		} 
+	
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Lançamento de Recarga");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+			
 		}
+		catch(IOException e) {
+			Alerts.showAlerts("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
 }
 
 
